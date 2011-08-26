6b47a859269ba8369b023e9d28171795eccf476b
Wesley Mason <wes@1stvamp.org>
2011-08-26 23:34:54 +0100

    Added emperor mode monitoring

--- Uwsgi.py
+++ Uwsgi.py
@@ -7,11 +7,30 @@ class Uwsgi:
         self.raw_config = raw_config
 
     def run(self):
-        return_data = {}
+        return_data = {'instances': 0}
         if self.config.get('UWSGI_EMPEROR_MODE', False):
-            pass
+            commands = {}
+            found_emperor = False
+            for command in [x.rstrip('\n').split(' ', 1) \
+              for x in subprocess.Popen(['ps', 'h', '-eo',
+                            'command', '|', 'grep', '-Fi', 'uwsgi'],
+                            stdout=subprocessPIPE).communicate()[0]]:
+                if command.startswith('grep '):
+                    continue
+                if not found_emperor and '--emperor' in command:
+                    found_emperor = True
+                if command not in commands:
+                    commands[command] = [command]
+                else:
+                    commands[command].append(command)
+            if not found_emperor:
+                self.logger.error('uWSGI emperor mode monitoring enabled'
+                        'but emperor instance not found')
+                return False
+            return_data['Apps'] = len(commands)
+            return_data['Average instances per app'] = sum(len(v) for k,v
+                    in commands.iteritems())
         else:
-            return_data['Instances up'] = 0
             return_data['Instances down'] = 0
             # If we know the address and port uwsgi is running on
             # try to ping it
@@ -25,7 +44,9 @@ class Uwsgi:
                     if code != 0:
                         return_data['Instances down'] += 1
                     else:
-                        return_data['Instances up'] += 1
+                        return_data['Instances'] += 1
             else:
                 pass
+
+
         return return_data
