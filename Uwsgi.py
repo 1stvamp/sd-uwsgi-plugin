99739126d3ce5a9f7de6881b1e39a31c59dd7c03
Wesley Mason <wes@1stvamp.org>
2011-08-26 17:51:29 +0100

    Added uwsgi --ping monitoring

--- Uwsgi.py
+++ Uwsgi.py
@@ -1,3 +1,5 @@
+import subprocess
+
 class Uwsgi:
     def __init__(self, config, logger, raw_config):
         self.config = agent_config
@@ -5,4 +7,25 @@ class Uwsgi:
         self.raw_config = raw_config
 
     def run(self):
-        return {}
+        return_data = {}
+        if self.config.get('UWSGI_EMPEROR_MODE', False):
+            pass
+        else:
+            return_data['Instances up'] = 0
+            return_data['Instances down'] = 0
+            # If we know the address and port uwsgi is running on
+            # try to ping it
+            addresses = self.config.get('UWSGI_ADDRESSES', False)
+            if addresses:
+                for address in addresses:
+                    code = subprocess.call(
+                        ["/usr/bin/env uwsgi", "--ping", address],
+                        shell=True
+                    )
+                    if code != 0:
+                        return_data['Instances down'] += 1
+                    else:
+                        return_data['Instances up'] += 1
+            else:
+                pass
+        return return_data
